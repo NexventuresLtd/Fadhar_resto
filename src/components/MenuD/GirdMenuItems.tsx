52de535d453d330011c8ec44ef26b77991f1695c
lscblack <l.christian@alustudent.com>
2025-09-18 23:15:32 +0200

    Done With Updated

--- src/components/MenuD/GirdMenuItems.tsx
+++ src/components/MenuD/GirdMenuItems.tsx
@@ -1011,7 +1011,7 @@ const CustomerMenu: React.FC = () => {
                         <div className="text-gray-400 mb-4">
                             <Search size={48} className="mx-auto" />
                         </div>
-                        <h3 className="text-lg font-medium text-gray-900 mb-2">No Menu items found</h3>
+                        <h3 className="text-lg font-medium text-gray-900 mb-2">No Dishes Available</h3>
                         <p className="text-gray-600">Try adjusting your search or filter criteria</p>
                     </div>
                 )}
